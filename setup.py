f2e3e52a1e3530df432ef11c17f413522e645539
Victor Nakoryakov <nail.xx@gmail.com>
2014-12-23 16:13:07 +0300

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme_content = open("README.md").read()
 
 setup(
     name='pyinsales',
-    version='0.7.0',
+    version='0.7.1',
     description='InSales e-commerce platform API bindings',
     long_description=readme_content,
     author='Victor Nakoryakov',
